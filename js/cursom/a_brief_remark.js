7feadb9ae8b82f6555a9029831004e0bdbd08b7b
ymkiux <3255284101@qq.com>
2021-01-14 09:07:53 +0800

    Site updated: 2021-01-14 09:07:50

---
+++ js/cursom/a_brief_remark.js
@@ -0,0 +1,12 @@
+//一言
+function hitokoto() {
+    fetch('https://v1.hitokoto.cn').then(response => response.json())
+        .then(data => {
+            var hitokoto = document.getElementById('lc-hitokoto');
+            hitokoto.innerText = data.hitokoto;
+        })
+        .catch(e => {
+            var hitokoto = document.getElementById('lc-hitokoto');
+            hitokoto.innerText = "在未知的未来将要发生的事，一定有它的意义";
+        })
+}
