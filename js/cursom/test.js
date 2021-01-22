e048cb2a1b0d981037dcd0ac49b339df7a7d712a
ymkiux <3255284101@qq.com>
2021-01-22 09:22:15 +0800

    Site updated: 2021-01-22 09:22:13

---
+++ js/cursom/test.js
@@ -0,0 +1,9 @@
+// var url = "http://localhost:4000/archives/";
+// axios.get(url).then((resp) => {
+//     console.log("请求成功，处理resp.data中的html数据");
+//     console.log(resp.request);
+//     // console.log(resp.body);
+// }).catch((err) => {
+//     console.log("请求失败，错误处理");
+// })
+
