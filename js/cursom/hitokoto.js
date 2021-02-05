a09004a624bc45273764c8be43a6a30b93cd813d
ymkiux <3255284101@qq.com>
2021-02-05 20:27:10 +0800

    Site updated: 2021-02-05 20:26:55

--- js/cursom/hitokoto.js
+++ js/cursom/hitokoto.js
@@ -1,18 +1,17 @@
 !function (e) {
     $(document).ready(function () {
-        fetch('https://v1.hitokoto.cn').then(response => response.json())
-            .then(data => {
-                //     var dom = document.querySelector('#div-hitokoto');
-                //     Array.isArray(dom) ? dom[0].innerText = hitokoto : dom.innerHTML = "<div id=\"lc-hitokoto\" style=\"margin-left:10px;margin-right:10px; \">" + data.hitokoto + "</div>";
-                $("#div-hitokoto").html("<div id=\"lc-hitokoto\" style=\"margin-left:10px;margin-right:10px; \">" + data.hitokoto + "</div>");
+        fetch("https://v1.hitokoto.cn?encode=json")
+            .then(function (response) {
+                return response.json();
             })
-        $("#div-hitokoto").mouseover(function () {
+            .then(function (data) {
+                $("#jinrishici-hitokoto").html("<div id=\"lc-hitokoto\" style=\"margin-left:10px;margin-right:10px; \">" + data.hitokoto + "</div>");
+            })
+        $("#jinrishici-hitokoto").mouseover(function () {
             $("#lc-hitokoto").css("color", "#c74f00");
-            // $("#lc-hitokoto").css("border-bottom", "1px dashed #c74f00");
         })
-        $("#div-hitokoto").mouseout(function () {
+        $("#jinrishici-hitokoto").mouseout(function () {
             $("#lc-hitokoto").css("color", "#666");
-            //  $("#lc-hitokoto").css("border-bottom", "0px dashed #c74f00");
         })
     });
 }(window);
